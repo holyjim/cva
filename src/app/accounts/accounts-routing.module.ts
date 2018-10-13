5d70bc4aa7c2f078e5efb1fd9e98ce831729b335
Ashwin Venkatesan <ashwinjv@gmail.com>
2018-10-12 20:53:54 -0700

    add route in accounts module for register component

--- src/app/accounts/accounts-routing.module.ts
+++ src/app/accounts/accounts-routing.module.ts
@@ -2,7 +2,10 @@ import { RegisterComponent } from './register/register.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: 'register', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
