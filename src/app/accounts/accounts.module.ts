bc68a78c18c752fd8ad2653169fa2d7b57c3f167
Ashwin Venkatesan <ashwinjv@gmail.com>
2018-10-12 20:53:17 -0700

    add register component to accounts module

--- src/app/accounts/accounts.module.ts
+++ src/app/accounts/accounts.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AccountsRoutingModule } from './accounts-routing.module';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AccountsRoutingModule,
   ],
-  declarations: []
+  declarations: [RegisterComponent]
 })
 export class AccountsModule { }
