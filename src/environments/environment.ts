287744d8b20a80a7af3fb0657d5c161bfef35820
Ashwin Venkatesan <ashwinjv@gmail.com>
2018-10-13 21:10:44 -0700

    add firebase config to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,14 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: 'AIzaSyCw3vY5hQbnwJZh2Uf-ANKrrAdwJmwne7o',
+    authDomain: 'cva-prod.firebaseapp.com',
+    databaseURL: 'https://cva-prod.firebaseio.com',
+    projectId: 'cva-prod',
+    storageBucket: 'cva-prod.appspot.com',
+    messagingSenderId: '357229182537',
+  },
 };
 
 /*
