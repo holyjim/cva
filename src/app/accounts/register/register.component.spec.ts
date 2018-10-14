3a682e95641f5e6434551942d45f933931314c83
Ashwin Venkatesan <ashwinjv@gmail.com>
2018-10-13 21:10:18 -0700

    Add accounts service to register component with a test register form

--- src/app/accounts/register/register.component.spec.ts
+++ src/app/accounts/register/register.component.spec.ts
@@ -1,14 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { AccountsService } from '../accounts.service';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let accountsServiceStub: any;
 
   beforeEach(async(() => {
+    accountsServiceStub = {
+      register: jasmine
+        .createSpy('register')
+        .and
+        .callFake((account) => account),
+    };
     TestBed.configureTestingModule({
       declarations: [ RegisterComponent ],
+      providers: [
+        RegisterComponent,
+        { provide: AccountsService, useValue: accountsServiceStub },
+      ],
     })
     .compileComponents();
   }));
--- src/app/accounts/register/register.component.ts
+++ src/app/accounts/register/register.component.ts
@@ -1,4 +1,6 @@
+import { AccountsService } from './../accounts.service';
 import { Component, OnInit } from '@angular/core';
+import { Department, AccountRole } from '../accounts.model';
 
 @Component({
   selector: 'cva-register',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private accountsService: AccountsService) { }
 
   ngOnInit() {
   }
 
+  register(email: string) {
+    this.accountsService.register({
+      email: email ? email : 'test@test.com',
+      displayName: 'Test',
+      department: Department.QA,
+      role: AccountRole.Participant,
+    }, 'password');
+  }
 }
