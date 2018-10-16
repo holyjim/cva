77c79dc8cd014bcea7b03656134ca50eea688098
Ashwin Venkatesan <ashwinjv@gmail.com>
2018-10-15 22:28:48 -0700

    add login component ui stub

--- src/app/accounts/login/login.component.ts
+++ src/app/accounts/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountsService } from '../accounts.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'cva-login',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private accountsService: AccountsService, public fb: FormBuilder) { }
 
   ngOnInit() {
+    // First Step
+    this.loginForm = this.fb.group({
+      'email': ['', [
+        Validators.required,
+        Validators.email,
+        ],
+      ],
+      'password': ['', [
+        Validators.required,
+        ],
+      ],
+    });
+  }
+  get email() { return this.loginForm.get('email'); }
+  get password() { return this.loginForm.get('password'); }
+
+  async register() {
+    await this.accountsService.login(
+      this.email.value,
+      this.password.value
+    );
   }
 
 }
