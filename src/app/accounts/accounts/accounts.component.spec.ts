b74244afc9691ddf0881ed6f59218d462f936699
Ashwin Venkatesan <ashwinjv@gmail.com>
2018-10-15 22:43:44 -0700

    add router and material module to accounts tests to get it to pass

--- src/app/accounts/accounts/accounts.component.spec.ts
+++ src/app/accounts/accounts/accounts.component.spec.ts
@@ -1,6 +1,9 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './../../app.material.module';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AccountsComponent } from './accounts.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AccountsComponent', () => {
   let component: AccountsComponent;
@@ -8,6 +11,11 @@ describe('AccountsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        BrowserAnimationsModule,
+        AppMaterialModule,
+      ],
       declarations: [ AccountsComponent ],
     })
     .compileComponents();
