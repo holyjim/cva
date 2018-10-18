672c5b26d5eca37e780652ca219beaef2bbab198
Ervin Ebalo <eebalo@gmail.com>
2018-10-17 20:03:24 -0700

    login - add constructor - login: auth to firebase

--- src/app/accounts/accounts.service.ts
+++ src/app/accounts/accounts.service.ts
@@ -3,12 +3,27 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestoreDocument, AngularFirestore } from '@angular/fire/firestore';
 
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root',
 })
 export class AccountsService {
 
-  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) { }
+  account: Observable<Account>;
+
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) { 
+    this.account = this.afAuth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.afs.doc<Account>(`accounts/${user.uid}`).valueChanges()
+        } else {
+          return of(null);
+        }
+      })
+    )
+  }
 
   async updateAccount(account: Account) {
     const accountRef: AngularFirestoreDocument<any> = this.afs.doc(`accounts/${account.uid}`);
@@ -27,6 +42,13 @@ export class AccountsService {
   }
 
   async login(email: string, password: string) {
-
+    try {
+      await this.afAuth.auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+    // redirect to somewhere
+    console.log('logged in!');
   }
 }
