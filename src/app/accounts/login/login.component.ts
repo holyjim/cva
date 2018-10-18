e94f4874f8abfc826291a8bf28f3f032ac28d385
Ervin Ebalo <eebalo@gmail.com>
2018-10-17 20:29:18 -0700

    removed email validator for login

--- src/app/accounts/login/login.component.ts
+++ src/app/accounts/login/login.component.ts
@@ -16,8 +16,7 @@ export class LoginComponent implements OnInit {
     // First Step
     this.loginForm = this.fb.group({
       'email': ['', [
-        Validators.required,
-        Validators.email,
+        Validators.required
         ],
       ],
       'password': ['', [
