dbf5673e2e4c4d1470b16cd582c7f57cedcf4d7e
Ervin Ebalo <eebalo@gmail.com>
2018-10-17 20:26:10 -0700

    Notify Service creates the message, clears them

---
+++ src/app/core/notify.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs'
+
+export interface Msg {
+  title: string;
+  content: string;
+  style: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotifyService {
+
+  private _msgSource = new Subject<Msg>();
+
+  msg = this._msgSource.asObservable();
+
+  constructor() { }
+
+  update(title: string, content: string, style: string) {
+    const msg: Msg = { title, content, style };
+    this._msgSource.next(msg);
+  }
+  clear() {
+    this._msgSource.next(null)
+  }
+}
