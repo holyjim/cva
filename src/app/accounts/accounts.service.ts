6a19dd7f7a8fc4b702aa6d12b34a342f5009d154
Ervin Ebalo <eebalo@gmail.com>
2018-10-17 20:28:27 -0700

    account service - added try/catch to login and register - added generic error handler - called notifyservice

--- src/app/accounts/accounts.service.ts
+++ src/app/accounts/accounts.service.ts
@@ -2,6 +2,7 @@ import { Account } from './accounts.model';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestoreDocument, AngularFirestore } from '@angular/fire/firestore';
+import { NotifyService } from '../core/notify.service';
 
 import { Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
@@ -13,7 +14,9 @@ export class AccountsService {
 
   account: Observable<Account>;
 
-  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) { 
+  constructor(private afAuth: AngularFireAuth, 
+              private afs: AngularFirestore,
+              private notify: NotifyService) { 
     this.account = this.afAuth.authState.pipe(
       switchMap(user => {
         if (user) {
@@ -31,24 +34,40 @@ export class AccountsService {
   }
 
   async register(account: Account, password: string): Promise<Account> {
-
-    const credential = await this.afAuth.auth.createUserWithEmailAndPassword(
-      account.email,
-      password
-    );
-    account.uid = credential.user.uid;
-    await this.updateAccount(account);
-    return account;
+    // try to create a user
+    try {
+      const credential = await this.afAuth.auth.createUserWithEmailAndPassword(
+        account.email,
+        password
+      );
+      account.uid = credential.user.uid;
+    } catch (error) {
+      this.handleError(error);
+      return;
+    }
+    
+    try {
+      await this.updateAccount(account);
+      return account;
+    } catch (error) {
+      this.handleError(error);
+    }
   }
 
   async login(email: string, password: string) {
     try {
       await this.afAuth.auth.signInWithEmailAndPassword(email, password);
     } catch (error) {
-      console.log(error);
+      this.handleError(error);
       return;
     }
     // redirect to somewhere
     console.log('logged in!');
+  }
+
+  // If error, console log and notify user
+  private handleError(error) {
+    console.error(error)
+    this.notify.update('Error', error.message, 'error')
   }
 }
